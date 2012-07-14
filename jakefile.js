3a524fff923c1153f40921c320ffe8a9d2423fcb
Michael Allan <michaelallan@optushome.com.au>
2012-07-14 11:13:21 +1000

    Add placeholder default jake task

--- jakefile.js
+++ jakefile.js
@@ -1,3 +1,7 @@
+task( "default", [], function () {
+    console.log( "default" );
+});
+
 desc( "An example" );
 task( "example", [ "dependency" ], function () {
     console.log( "example task in jake" );
